2ac5510da0841bd4e8337996ca4b8387c5655722
Ayoub Mahmoud <116462151+ayoubxmahmoud@users.noreply.github.com>
2024-10-20 23:26:23 +0100

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,20 +1,21 @@
-import logo from './logo.png'
-import add_icon from './add_icon.png'
-import order_icon from './order_icon.png'
-import profile_image from './profile_image.png'
-import upload_area from './upload_area.png'
-import parcel_icon from './parcel_icon.png'
-import list from './list.png'
-import default_avatar from './default-avatar.png'
-export const assets ={
-    logo,
-    add_icon,
-    order_icon,
-    profile_image,
-    upload_area,
-    parcel_icon,
-    list,
-    default_avatar
-}
+import logo from './logo.png';
+import addIcon from './add_icon.png';
+import orderIcon from './order_icon.png';
+import profileImage from './profile_image.png';
+import uploadArea from './upload_area.png';
+import parcelIcon from './parcel_icon.png';
+import list from './list.png';
+import defaultAvatar from './default-avatar.png';
+
+export const assets = {
+  logo,
+  addIcon,
+  orderIcon,
+  profileImage,
+  uploadArea,
+  parcelIcon,
+  list,
+  defaultAvatar,
+};
 
 export const url = 'https://food-express-backend-tr7v.onrender.com'
