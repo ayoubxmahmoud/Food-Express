5b5ddf3e79c028997cd17a85bf1e737866566a47
Ayoub Mahmoud <116462151+ayoubxmahmoud@users.noreply.github.com>
2024-10-22 19:33:07 +0100

    Update admin-popover.tsx

--- admin/src/components/dashboard/layout/admin-popover.tsx
+++ admin/src/components/dashboard/layout/admin-popover.tsx
@@ -57,7 +57,7 @@ export function AdminPopover({ anchorEl, onClose, open }: AdminPopoverProps): Re
   };
   
   React.useEffect(() => {
-    loadAdminData();
+    void loadAdminData();
   }, [])
 
   const router = useRouter();
